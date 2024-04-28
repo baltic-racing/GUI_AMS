0003aea04411153e8418ba619978a2e55ae4c8e3
Valborian <62020204+Valborian@users.noreply.github.com>
2024-04-28 22:37:07 +0200

    ASCII to int

---
+++ fuck.py
@@ -0,0 +1,7 @@
+liste = [1,2,3,4,5,6,7,8]
+
+for x in liste:
+    print(x)
+
+def gruesse():
+    print("hallo welt")
--- import_serial.py
+++ import_serial.py
@@ -1,7 +1,34 @@
 import serial
+
+from fuck import gruesse
 var = serial.Serial()
-var.port = "/dev/ttyACM0"
+#var.port = "/dev/ttyACM0"
+var.port = "COM3"
 var.open()
+
+size = 18
+
 while(1):
-    print(var.readline())
+    gelesene_bytes = var.read_until(b"\0")
+    print(bytes(gelesene_bytes))
+    if len(gelesene_bytes) != 12:
+        print("Katastrophe")
+        continue
+
+    messwerte = [x for x in bytes(gelesene_bytes) if x != 0]
+    print(messwerte)
+
+    neue_messwerte = []
+    for zahl in gelesene_bytes:
+        #print(zahl)
+        if zahl != 0:
+            neue_messwerte.append(zahl)
+
+    print(neue_messwerte)
+    gruesse()
+
+    #print("Hello World")
+
+    #for i in range(size):
+        #print("Test ", i, ": ", var.readline())
 
