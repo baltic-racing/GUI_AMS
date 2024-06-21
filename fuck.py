c77c930740e2d83141f4c30a18ecb9615f360cbf
Aurvandill <cerberus885@gmail.com>
2024-06-21 15:57:49 +0200

    rewrite angefangen

--- fuck.py
+++
@@ -1,7 +0,0 @@
-liste = [1,2,3,4,5,6,7,8]
-
-for x in liste:
-    print(x)
-
-def gruesse():
-    print("hallo welt")
--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import serial
 import asyncio
+from pathlib import Path
+from sanic import Sanic, Request
+from sanic.response import json, html
+from typing import Optional
 
+app = Sanic("MyHelloWorldApp")
+
+#globale Defines
 NUM_STACK = 12
-
-
-
 NUM_CELLS_STACK = 12
 NUM_CELLS = NUM_STACK * NUM_CELLS_STACK
 usb_data_size = NUM_CELLS * 2 + 1
 
-from pathlib import Path
-from sanic import Sanic, Request
-from sanic.response import json, html
-
-
-app = Sanic("MyHelloWorldApp")
-
-#stack_voltages_max = [1, 2, 3, 4, 5, 6, 7, 8]
-#stack_temperatures_max = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8]
-
-#stack_voltages_min = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
-#stack_temperatures_min = [1, 2, 3, 4, 5, 6, 7, 8]
-
-#detailed_stack_info_voltage = [x for x in range(144)]
-#detailed_stack_info_temperature = [x for x in range(144)]
+# globale Status Variablen
+connected = False
+connected_port = "?"
+serial_connection: Optional[serial.Serial] = None
 
 stack_voltages_max = [x for x in bytes(NUM_STACK)]
 stack_temperatures_max = [x for x in bytes(NUM_STACK)]
@@ -36,8 +29,58 @@ detailed_stack_info_temperature = [x for x in bytes(NUM_CELLS)]
 
 sum_voltage = [x for x in bytes(NUM_STACK)]
 
+
+@app.post("/connection")
+async def init_connection(request:Request):
+    global serial_connection
+    global connected_port
+    global connected
+    if connected:
+        return json({"error": "Verbindung ist bereits aufgebaut"},400)
+    try:
+        json_data:dict = request.json
+        if json_data.get("baudrate",None) is None:
+            json_data["baudrate"] = 9600
+        if json_data.get("bytesize",None) is None:
+            json_data["bytesize"] = 8
+        if json_data.get("port", None) is None:
+            return json({"error":"kein Port angegeben"},400)
+        serial_connection = serial.Serial(**json_data)
+        serial_connection.open()
+        connected_port = json_data["port"]
+    except serial.SerialException as err:
+        serial_connection = None
+        return json({"error": f"SerialException: {str(err)}"})
+    except Exception as err:
+        serial_connection = None
+        return json({"error":f"{type(err)} - {str(err)}"})
+    
+    connected = True
+    return json({"msg":"Verbindung aufgebaut"})
+
+@app.delete("/connection")
+async def destroy_connection(request:Request):
+    global connected
+    global serial_connection
+
+    if not connected:
+        return json({"error":"Es ist keine Verbindung aufgebaut!"})
+    
+    serial_connection.close()
+    serial_connection = None
+    connected = False
+    return json({"msg":"Verbindung getrennt"})
+
+@app.get("/connection")
+async def get_connection_info(request:Request):
+    global connected
+    global connected_port
+
+    return json({"connected":connected, connected_port:connected_port})
+
+
 @app.get("/styles.css")
-async def style(request):
+async def style(request:Request):
     return html(Path("./beer.min.css").read_text())
 
 
@@ -46,10 +89,15 @@ async def hello_world(request):
     return html(Path("./main.html").read_text())
 
 
+@app.get("/stack/count")
+async def get_stack_count(request: Request):
+    return json({"stacks": NUM_STACK})
+
+
 @app.get("/stack/detailed/<stack_num:int>")
 async def stack_info_detail(request: Request, stack_num: int):
-    #print(detailed_stack_info_voltage)
-    #print(detailed_stack_info_temperature)
+    # print(detailed_stack_info_voltage)
+    # print(detailed_stack_info_temperature)
     offset = stack_num * 12
 
     cell_voltages = detailed_stack_info_voltage[offset : offset + 12]
@@ -60,7 +108,7 @@ async def stack_info_detail(request: Request, stack_num: int):
 
 @app.get("/stack/<stack_num:int>")
 async def stack_info(request: Request, stack_num: int):
-    #print(stack_voltages_max)
+    # print(stack_voltages_max)
     try:
         return json(
             {
@@ -85,49 +133,58 @@ async def stack_info(request: Request, stack_num: int):
 
 def get_data(serial_interface):
     gelesene_bytes = serial_interface.read_until(b"\xff")
-    #messwerte = [x for x in bytes(gelesene_bytes) if x != 0]
+    # messwerte = [x for x in bytes(gelesene_bytes) if x != 0]
     messwerte = [x for x in bytes(gelesene_bytes)]
     return messwerte
 
+
 async def data_task():
-    #global stack_voltages_max
+    # global stack_voltages_max
     global detailed_stack_info_voltage
     global detailed_stack_info_temperature
     global sum_voltage
+    global connected
+    global serial_connection
 
-    # test = "lol"
-    var = serial.Serial()
-    #var.port = "/dev/ttyACM0"
-    var.port = "COM3"
-    var.open()
     while True:
-        messwerte = await asyncio.get_event_loop().run_in_executor(None, get_data, var)
-        #stack_voltages_max = messwerte
-        #print("wtf:", messwerte)
-        detailed_stack_info_voltage = [messwerte[x] for x in range(NUM_CELLS)]
-        #print("dafuq:", detailed_stack_info_voltage)
-        detailed_stack_info_temperature = [messwerte[x + NUM_CELLS] for x in range(NUM_CELLS)]
-        #print("Temp: ",detailed_stack_info_temperature)
-        #stack_voltages_max =  max(detailed_stack_info_voltage[12:24])
-        for i in range(0 , NUM_CELLS, 12):
-            max_value_voltage = max(detailed_stack_info_voltage[i:i + NUM_CELLS_STACK])
-            stack_voltages_max[i // 12] = max_value_voltage/10
+        if connected:
+            messwerte = await asyncio.get_event_loop().run_in_executor(
+                None, get_data, serial_connection
+            )
+            # stack_voltages_max = messwerte
+            # print("wtf:", messwerte)
+            detailed_stack_info_voltage = [messwerte[x] for x in range(NUM_CELLS)]
+            # print("dafuq:", detailed_stack_info_voltage)
+            detailed_stack_info_temperature = [
+                messwerte[x + NUM_CELLS] for x in range(NUM_CELLS)
+            ]
+            # print("Temp: ",detailed_stack_info_temperature)
+            # stack_voltages_max =  max(detailed_stack_info_voltage[12:24])
+            for i in range(0, NUM_CELLS, 12):
+                max_value_voltage = max(
+                    detailed_stack_info_voltage[i : i + NUM_CELLS_STACK]
+                )
+                stack_voltages_max[i // 12] = max_value_voltage / 10
 
-            min_value_voltage = min(detailed_stack_info_voltage[i:i + NUM_CELLS_STACK - 1])
-            stack_voltages_min[i // 12] = min_value_voltage/10
+                min_value_voltage = min(
+                    detailed_stack_info_voltage[i : i + NUM_CELLS_STACK - 1]
+                )
+                stack_voltages_min[i // 12] = min_value_voltage / 10
 
-            max_value_temperature = max(detailed_stack_info_temperature[i:i + NUM_CELLS_STACK])
-            stack_temperatures_max[i // 12] = max_value_temperature
+                max_value_temperature = max(
+                    detailed_stack_info_temperature[i : i + NUM_CELLS_STACK]
+                )
+                stack_temperatures_max[i // 12] = max_value_temperature
 
-            min_value_temperature = min(detailed_stack_info_temperature[i:i + NUM_CELLS_STACK - 1])
-            stack_temperatures_min[i // 12] = min_value_temperature
+                min_value_temperature = min(
+                    detailed_stack_info_temperature[i : i + NUM_CELLS_STACK - 1]
+                )
+                stack_temperatures_min[i // 12] = min_value_temperature
 
-            sum_stack_voltage = sum(detailed_stack_info_voltage[i:i + NUM_CELLS_STACK])
-            sum_voltage[i // 12] = sum_stack_voltage/10
-
-        #print("max :", stack_voltages_max)
-        #print("min :", stack_voltages_min)
-        #print("test: ", sum_voltage)
+                sum_stack_voltage = sum(
+                    detailed_stack_info_voltage[i : i + NUM_CELLS_STACK]
+                )
+                sum_voltage[i // 12] = sum_stack_voltage / 10
 
 
 app.add_task(data_task)
